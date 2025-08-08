073f4b8dcffc71742ec3e97f88e53335aa68941e
Allenpt221 <Allenpt26@gmail.com>
2025-08-08 19:06:18 +0800

    install the framer motion for the responsive design for the website

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,49 +1,8 @@
-import Link from 'next/link';
-import React from 'react';
-import { Input } from './ui/input';
-import { Search } from 'lucide-react';
+// components/Navbar.tsx
 import { getProfile } from '@/server-action/auth.action';
+import NavbarClient from './NavbarClient';
 
-async function Navbar() {
-
+export default async function Navbar() {
   const profile = await getProfile();
-  
-  console.log(profile);
-
-  return (
-    <div className='shadow-sm py-2 bg-white'>
-      <div className='flex justify-between items-center px-10'>
-        <Link href="/" className='text-xl font-bold'>
-          LMS
-        </Link>
-        <div className='flex space-x-5 font-medium'>
-          <div className="relative w-[30rem]">
-            <Input
-              type="text"
-              placeholder="Search..."
-              className="h-8 pr-10"
-            />
-            <Search className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 w-5 h-5" />
-          </div>
-          {profile?.success ? (
-            <div className='flex items-center'>
-              <p className='font-normal text-sm'>{profile.user?.username}</p>
-              <img src={profile.user?.image} alt="error fetching profile" className='w-10 h-10' />
-            </div>
-          ) : (
-            <div>
-              <Link href='/login' className='border px-4 py-1 rounded-full'>
-                Log In
-              </Link>
-              <Link href='/register' className='px-4 py-1 rounded-full bg-black text-white hover:bg-black/60 transition-all duration-100 ease-in'>
-                Sign up
-              </Link>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+  return <NavbarClient profile={profile} />;
 }
-
-export default Navbar;
