d99e5565a75dc7704f247e015cb2bd3069cb70ec
Allenpt221 <Allenpt26@gmail.com>
2025-08-09 17:25:57 +0800

    add and post in the table

---
+++ prisma/migrations/20250809092510_update_user_post_model/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "public"."Post" (
+    "id" TEXT NOT NULL,
+    "AuthorId" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "postId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."Post" ADD CONSTRAINT "Post_AuthorId_fkey" FOREIGN KEY ("AuthorId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
