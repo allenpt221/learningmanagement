aa601301064363fc5ffc1ddcaa1b1a08f299fb7f
Allenpt221 <Allenpt26@gmail.com>
2025-08-14 16:29:52 +0800

    ...

---
+++ src/components/Modal/FollowerModal.tsx
@@ -0,0 +1,48 @@
+'use client'
+import React, { useState } from 'react';
+import { X } from 'lucide-react';
+import Link from 'next/link';
+import FollowButton from '@/components/FollowButton';
+
+interface FollowerModalProps {
+  followers: any[]; 
+  isOpen: boolean;
+  isClose: () => void;
+  profile: any; 
+}
+
+function FollowerModal({ followers, isOpen, isClose, profile }: FollowerModalProps) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+      <div className="bg-white p-4 w-[30rem] rounded-lg h-fit max-h-[30rem] overflow-hidden">
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-lg font-semibold">Followers</h1>
+          <button onClick={isClose}><X /></button>
+        </div>
+
+        <div className="flex flex-col gap-3 max-h-96 overflow-y-auto">
+          {followers.map((user) => (
+            <div key={user.id} className="flex items-center justify-between">
+              <Link href={`${user.username}`} className="flex items-center space-x-3">
+                <img
+                  src={user.image}
+                  alt={user.username}
+                  className="w-10 h-10 rounded-full object-cover"
+                />
+                <div>
+                  <p className="font-medium text-sm">{user.username}</p>
+                  <p className="text-gray-500 text-xs">{user.firstname} {user.lastname}</p>
+                </div>
+              </Link>
+
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FollowerModal;
