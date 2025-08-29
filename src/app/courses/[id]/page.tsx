088e459cc48f8957b3990176183173e4f2024437
Allenpt221 <Allenpt26@gmail.com>
2025-08-29 18:54:38 +0800

    make it page

--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -27,7 +27,7 @@ export async function generateMetadata({
   };
 }
 
-export default function CoursePage({ params }: { params: { id: string } }) {
+export default function Page({ params }: { params: { id: string } }) {
   const course = mapCourses.find((c) => c.id === params.id);
 
   if (!course) {
