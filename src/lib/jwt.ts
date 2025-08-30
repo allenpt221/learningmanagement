278b61ef8e8fe7ccffea843d1e7b0d8e2866234d
Allenpt221 <Allenpt26@gmail.com>
2025-08-30 16:15:23 +0800

    new update

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,10 +1,11 @@
 import jwt from 'jsonwebtoken';
 
-const JWT_SECRET = process.env.JWT_SECRET as string;
 
-if (!JWT_SECRET) {
+if (!process.env.JWT_SECRET) {
   throw new Error("JWT_SECRET is not defined in environment variables.");
 }
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 interface TokenPayload {
   userId: string;
